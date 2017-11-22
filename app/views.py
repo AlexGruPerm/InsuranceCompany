e10fc1bedfbfae4484016df7a084abbcf93acb48
AlexGruPerm <ugr@bk.ru>
2017-11-22 13:40:15 +0500

    Dict as input to views.

--- app/views.py
+++ app/views.py
@@ -5,4 +5,13 @@ from flask import render_template
 @app.route('/index')
 def index():
     #return "Hello, World! 123"
-    return render_template('base.html')
+    #return render_template('base.html')
+    user = {'nickname': 'Miguel2'}  # выдуманный пользователь
+    users= {}
+    for i in range(1,15,1):
+        #users["k"+str(i)]="First Name_"+str(i)
+        users["k" + str(i)] = {"FirstName":"First Name_" + str(i),"Surname":"Surname_"+str(i*10)}
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           users=users)
